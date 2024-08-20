8fd5b82e3c58f75876a9879f4c302633083a620e
laohu <laohuangnextdoor@outlook.com>
2024-08-20 15:56:41 +0800

    Revert "makefile"
    This reverts commit 6eac43a17bfd215438ae018b313f9171f11d918d.

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,6 @@ import java.io.FileNotFoundException;
 public class Main {
     public static void main(String[] args) throws Exception {
         //String name = "test.mx";
-        //InputStream input = new FileInputStream(name);
         InputStream input = System.in;
 
         try {
