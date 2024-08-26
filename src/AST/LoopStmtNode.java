bccec6f8fb7d1667cfc6ad243f7357cb9b6637a4
laohu <laohuangnextdoor@outlook.com>
2024-08-26 17:38:03 +0800

    builder已经能构建出正确的AST

--- src/AST/LoopStmtNode.java
+++ src/AST/LoopStmtNode.java
@@ -2,7 +2,7 @@ package AST;
 
 import Util.position;
 
-abstract public class LoopStmtNode extends ASTNode {
+abstract public class LoopStmtNode extends StmtNode {
     public LoopStmtNode (position pos) {
         super (pos) ;
     }
