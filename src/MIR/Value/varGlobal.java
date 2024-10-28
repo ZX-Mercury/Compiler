4594959afc86b2eac9b130c1bfbca7c6644e64db
laohu <laohuangnextdoor@outlook.com>
2024-10-28 15:54:09 +0800

    IR: add type & value

--- src/MIR/Value/varGlobal.java
+++ src/MIR/Value/varGlobal.java
@@ -1,4 +1,12 @@
 package MIR.Value;
 
-public class varGlobal {
+import MIR.Type.*;
+
+public class varGlobal extends value {
+    public String name;
+
+    public varGlobal(String name, type type) {
+        super(type);
+        this.name = name;
+    }
 }
